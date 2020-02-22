c2de953fbeb5cff002382459a6252056133dd0ab
Saravanan <sprabhakar@ztech.io>
2020-02-23 00:14:14 +0530

    added a new dummy module

---
+++ app/code/ABC/Hello/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * dasdasdasjdasjdhasjkhdkjsahdkjsahdjksahkjdhsakjdhasjkhdjkashjdkhasjkdhasjkdhkasjhdkjashdjkashjkdashkjdhsajkdhasjkhdasjkhdjsakhdjkashdjkashdjkashkjdhasjkdhsajkdhasjkhdasjkhdjsakhdjksahdjkashjdkhasjkdhasjkhdjaskhdjskh
+**/
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ABC_Hello',
+    __DIR__
+);
